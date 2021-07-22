e0c005b756ae19fc12ffd24033406c3d7e9cbc3c
khuatthimylinh <ktml20112000@gmail.com>
2021-07-22 20:46:27 +0700

    first commit

---
+++ blog/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace blog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Search",
+                url: "tim-kiem",
+                defaults: new { controller = "Category", action = "Search", id = UrlParameter.Optional }
+               
+            );
+        }
+    }
+}
---
+++ blog/Models/searchAcount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+
+namespace blog.Models
+    
+{
+    public class searchAcount
+    {
+        public BlogDataEntities4 db = null;
+        public IEnumerable<Account>ListAllPaging(int page, int pageSize)
+        {
+            return db.Accounts.ToPagedList(page,pageSize);
+        }
+       
+    }
+}
